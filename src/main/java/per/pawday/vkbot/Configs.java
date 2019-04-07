87f59be22371bd6cf480a4877b7a09c479be3da3
Pawday <pawday@mail.ru>
2019-04-07 13:10:22 +0400

    Configs: database's configs.

--- src/main/java/per/pawday/vkbot/Configs.java
+++ src/main/java/per/pawday/vkbot/Configs.java
@@ -26,7 +26,7 @@ class Configs
 
     //files
     private final static File configsToken = new File(configsDir.getName().concat("/token.json"));
-
+    private final static File configsDatabase = new File(configsDir.getName().concat("/database.json"));
 
 
     static
@@ -43,7 +43,6 @@ class Configs
         writeToFiles();
 
         setParams();
-
 
         removeFiles();
 
@@ -72,6 +71,25 @@ class Configs
                 System.exit(1);
             }
         }
+        //database.json
+        if (! configsDatabase.exists())
+        {
+
+            try
+            {
+                configsDatabase.createNewFile();
+                FileOutputStream out = new FileOutputStream(configsDatabase);
+
+                out.write(JsonFormatter.formatToString(configs.database.databaseObject.toJSONString(),"\t").getBytes());
+                out.close();
+
+            } catch (IOException e)
+            {
+                System.out.println(ConsoleColors.RED + configsDatabase.getName() + "/token.json");
+                e.printStackTrace();
+                System.exit(1);
+            }
+        }
 
     }
 
@@ -89,7 +107,7 @@ class Configs
 
                 if (fileObject.get("token") != null)
                 {
-                    configs.token.tokenObject.put("token",fileObject.get("token"));
+                    configs.token.tokenObject = fileObject;
                 }
             }
             catch (IOException e)
@@ -104,6 +122,65 @@ class Configs
                 e.printStackTrace();
                 System.exit(1);
             }
+
+        }
+
+        //database.json
+        {
+            try
+            {
+                InputStream stream = new FileInputStream(configsDatabase);
+                JSONObject fileObject = (JSONObject) parser.parse(new InputStreamReader(stream));
+                stream.close();
+
+                if
+                (
+                        fileObject.get("MySQL") != null &&
+                        fileObject.get("PostgreSQL") != null &&
+                        fileObject.get("SQLite") != null
+                )
+                {
+                    JSONObject mysql = (JSONObject) fileObject.get("MySQL");
+                    JSONObject postgresql = (JSONObject) fileObject.get("PostgreSQL");
+                    JSONObject sqlite = (JSONObject) fileObject.get("SQLite");
+
+                    if    //   ( ╯°□°)╯  ┻━━━━┻
+                    (
+
+                        mysql.get("uses") != null &&
+                        mysql.get("host") != null &&
+                        mysql.get("user") != null &&
+                        mysql.get("password") != null &&
+                        mysql.get("port") != null &&
+
+                        postgresql.get("uses") != null &&
+                        postgresql.get("host") != null &&
+                        postgresql.get("user") != null &&
+                        postgresql.get("password") != null &&
+                        postgresql.get("port") != null &&
+
+                        sqlite.get("uses") != null &&
+                        sqlite.get("path") != null
+                    )
+                    {
+                        configs.database.databaseObject = fileObject;
+                    }
+                }
+
+            }
+            catch (IOException e)
+            {
+                System.out.println(ConsoleColors.RED + configsDir.getName() + "/database.json");
+                e.printStackTrace();
+                System.exit(1);
+            }
+            catch (ParseException e)
+            {
+                System.out.println(ConsoleColors.RED + "File " + configsDir.getName() + "/" + configsToken.getName() + " is broken" + ConsoleColors.RESET);
+                e.printStackTrace();
+                System.exit(1);
+            }
+
 
         }
     }
@@ -130,6 +207,25 @@ class Configs
                 System.exit(1);
             }
         }
+        {
+            try
+            {
+                FileOutputStream out = new FileOutputStream(configsDatabase);
+                out.write(JsonFormatter.formatToString(configs.database.databaseObject.toJSONString(),"\t").getBytes(StandardCharsets.UTF_8));
+                out.close();
+            }
+            catch (FileNotFoundException e)
+            {
+                e.printStackTrace();
+                System.exit(1);
+            }
+            catch (IOException e)
+            {
+                System.out.println(ConsoleColors.RED + configsDir.getName() + "/database.json");
+                e.printStackTrace();
+                System.exit(1);
+            }
+        }
 
     }
 
@@ -139,32 +235,47 @@ class Configs
         {
             configs.token.token = (String) configs.token.tokenObject.get("token");
         }
+
+        //database.json
+        {
+
+            JSONObject mysql = (JSONObject) configs.database.databaseObject.get("MySQL");
+            JSONObject postgresql = (JSONObject) configs.database.databaseObject.get("PostgreSQL");
+            JSONObject sqlite = (JSONObject) configs.database.databaseObject.get("SQLite");
+
+            configs.database.MySQL.uses = (boolean) mysql.get("uses");
+            configs.database.MySQL.host = (String) mysql.get("host");
+            configs.database.MySQL.user = (String) mysql.get("user");
+            configs.database.MySQL.password = (String) mysql.get("password");
+            configs.database.MySQL.port = (String) mysql.get("port");
+
+            configs.database.PostgreSQL.uses = (boolean) postgresql.get("uses");
+            configs.database.PostgreSQL.host = (String) postgresql.get("host");
+            configs.database.PostgreSQL.user = (String) postgresql.get("user");
+            configs.database.PostgreSQL.password = (String) postgresql.get("password");
+            configs.database.PostgreSQL.port = (String) postgresql.get("port");
+
+            configs.database.SQLite.uses = (boolean) sqlite.get("uses");
+            configs.database.SQLite.path = (String) sqlite.get("path");
+        }
     }
 
     public static void removeFiles()
     {
-        File[] files = configsDir.listFiles();
-
-        for (int i = 0; i < files.length; i++)
-        {
-            System.out.println(files[i].delete());
-        }
+        configsToken.delete();
+        configsDatabase.delete();
     }
 
     public static void returnFiles()
     {
         createFiles();
-
-        prepareObjects();
-
         writeToFiles();
-
-        setParams();
     }
 
     public static class TheConfigs
     {
         public Token token = new Token();
+        public Database database = new Database();
 
 
 
@@ -174,8 +285,7 @@ class Configs
             private String token;
             private JSONObject tokenObject = new JSONObject();
 
-
-            public Token()
+            Token()
             {
                 tokenObject.put("token","");
             }
@@ -183,6 +293,134 @@ class Configs
             public String getToken()
             {
                 return token;
+            }
+
+
+        }
+
+        public static class Database
+        {
+            public MySQL MySQL = new MySQL();
+            public PostgreSQL PostgreSQL = new PostgreSQL();
+            public SQLite SQLite = new SQLite();
+
+            private JSONObject databaseObject = new JSONObject();
+
+            Database()
+            {
+                JSONObject mysql = new JSONObject();
+
+                mysql.put("uses",true);
+                mysql.put("host","");
+                mysql.put("user","");
+                mysql.put("password","");
+                mysql.put("port","");
+
+                databaseObject.put("MySQL",mysql);
+
+                JSONObject postgresql = new JSONObject();
+
+                postgresql.put("uses" , false);
+                postgresql.put("host" , "");
+                postgresql.put("user" , "");
+                postgresql.put("password" , "");
+                postgresql.put("port" , "");
+
+                databaseObject.put("PostgreSQL" , postgresql);
+
+
+                JSONObject sqlite = new JSONObject();
+
+                sqlite.put("uses" , false);
+                sqlite.put("path" , "");
+
+                databaseObject.put("SQLite",sqlite);
+
+            }
+
+
+            public static class MySQL
+            {
+                private boolean uses;
+                private String host;
+                private String user;
+                private String password;
+                private String port;
+
+                public boolean isUses()
+                {
+                    return uses;
+                }
+
+                public String getHost()
+                {
+                    return host;
+                }
+
+                public String getUser()
+                {
+                    return user;
+                }
+
+                public String getPassword()
+                {
+                    return password;
+                }
+
+                public String getPort()
+                {
+                    return port;
+                }
+            }
+
+            public static class PostgreSQL
+            {
+                private boolean uses;
+                private String host;
+                private String user;
+                private String password;
+                private String port;
+
+                public boolean isUses()
+                {
+                    return uses;
+                }
+
+                public String getHost()
+                {
+                    return host;
+                }
+
+                public String getUser()
+                {
+                    return user;
+                }
+
+                public String getPassword()
+                {
+                    return password;
+                }
+
+                public String getPort()
+                {
+                    return port;
+                }
+            }
+
+            public static class SQLite
+            {
+                private boolean uses;
+                private String path;
+
+                public boolean isUses()
+                {
+                    return uses;
+                }
+
+                public String getPath()
+                {
+                    return path;
+                }
             }
 
 
--- src/main/java/per/pawday/vkbot/Main.java
+++ src/main/java/per/pawday/vkbot/Main.java
@@ -4,6 +4,7 @@ package per.pawday.vkbot;
 
 
 
+import per.pawday.vkbot.console.ConsoleColors;
 import java.io.IOException;
 
 
@@ -11,6 +12,12 @@ class Main
 {
     public static void main(String[] args) throws IOException
     {
+        if (args.length == 1 && args[0].equals("init"))
+        {
+            Configs.returnFiles();
+            System.out.println(ConsoleColors.GREEN + "Make a setting up config files now." + ConsoleColors.RESET);
+            System.exit(0);
+        }
 
     }
 }
